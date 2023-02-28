0e5ba42e54206e523135f4e0e00599278999fe8b
Bevan Hunt <bevan@bevanhunt.com>
2023-02-27 20:29:16 -0800

    update text

--- utils/contentData.ts
+++ utils/contentData.ts
@@ -2,12 +2,22 @@ const contentData = [
   {
     title: 'Quick QR Generation',
     description:
-      'One front-end API call to generate a Solana Pay QR code rather than writing complex full-stack code.',
+      'Write one front-end API call to generate a Solana Pay QR code rather than complex full-stack code.',
   },
   {
     title: 'Real-time Notifications',
     description:
       'Receive websocket notifications when transactions complete rather than front-end polling.',
+  },
+  {
+    title: 'Super-fast Transactions',
+    description:
+      'Helius is the provider used to process transactions quickly and reliably.',
+  },
+  {
+    title: 'No Account Required',
+    description:
+      'Just connect to our API and start sending and receiving payments.',
   },
 ];
 

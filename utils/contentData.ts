e0db0d0921c9654d90ed3ffa3c2456e964a5d3ff
Bevan Hunt <bevan@bevanhunt.com>
2023-02-28 17:07:18 -0800

    update SEO

--- utils/contentData.ts
+++ utils/contentData.ts
@@ -2,7 +2,7 @@ const contentData = [
   {
     title: 'Quick QR Generation',
     description:
-      'Write one front-end API call to generate a Solana Pay QR code rather than complex full-stack code.',
+      'StableThread enables you to write one front-end API call rather than complex full-stack code.',
   },
   {
     title: 'Real-time Notifications',
